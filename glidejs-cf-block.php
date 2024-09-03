506bc0034f580c557d9940344ee1e47a2986e051
Jarkko Saltiola <jarkko@saltiolacode.fi>
2024-09-03 15:40:18 +0300

    Render block content using Timber/Twig WIP

--- glidejs-cf-block.php
+++ glidejs-cf-block.php
@@ -14,6 +14,7 @@
 
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
+use Timber\Timber;
 
 
 add_action( 'carbon_fields_register_fields', 'crb_attach_theme_options' );
@@ -24,26 +25,14 @@ function crb_attach_theme_options() {
 		Field::make( 'image', 'image', __( 'Block Image' ) ),
 		Field::make( 'rich_text', 'content', __( 'Block Content' ) ),
 	) )
-	->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
-		?>
+		->set_render_callback( function ( $fields, $attributes, $inner_blocks ) {
+			$context = Timber::get_context();
+			$context['fields'] = $fields;
+			// $context['image'] = get_image($context['fields']['image']);
+			$context['attributes'] = $attributes;
 
-		<div class="block">
-			<div class="block__heading">
-				<h1><?php echo esc_html( $fields['heading'] ); ?></h1>
-			</div><!-- /.block__heading -->
-
-			<div class="block__image">
-				<?php echo wp_get_attachment_image( $fields['image'], 'full' ); ?>
-			</div><!-- /.block__image -->
-
-			<div class="block__content">
-				<?php echo apply_filters( 'the_content', $fields['content'] ); ?>
-			</div><!-- /.block__content -->
-		</div><!-- /.block -->
-
-		<?php
+			echo Timber::render('glidejs-cf-block.twig', $context);
 	} );
-
 }
 
 add_action( 'after_setup_theme', 'crb_load' );
